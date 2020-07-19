1e84f83f458c72aa45ce83052c37758c30fa8563
Sean Yang <32950943+seanys@users.noreply.github.com>
2020-07-19 14:16:12 +0800

    更新实验结果

--- lp_search_opt.py
+++ lp_search_opt.py
@@ -33,7 +33,7 @@ zfill_num = 5
 
 class LPSearch(object):
     def __init__(self, **kw):
-        self.line_index = 6
+        self.line_index = 77
         self.max_time = 1800
         if "line_index" in kw:
             self.line_index = kw["line_index"]
